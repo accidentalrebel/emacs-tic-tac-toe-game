9bb598cfe199779f62d49af27bd185c76f17352f
accidentalrebel <accidentalrebel@gmail.com>
2016-05-30 23:45:08 +0800

    Created the place-point-at function
    Signed-off-by: accidentalrebel <accidentalrebel@gmail.com>

--- main.el
+++ main.el
@@ -3,12 +3,38 @@
   (interactive)
   (switch-to-buffer "tic-tac-toe")
   (setup-tic-tac-board)
+  (display-tic-tac-board)
   )
 
 (defun setup-tic-tac-board()
+  "Set ups the tic tac board with initial values"
   (setq tic-tac-board (make-vector 9 ?.))
+  )
+
+(defun display-tic-tac-board()
+  "Displays the board"
+  (insert "displaying the board")
+  (place-point-at 3 2)
   )
 
 (defvar-local tic-tac-board 
   "This Is the tic tac toe board vector"
+  )
+
+(defconst BOARD_COL_COUNT 3
+  "The number of columns for the board"
+  )
+
+(defconst BOARD_ROW_COUNT 3
+  "The number of rows for the board"
+  )
+
+;; DISPLAY
+(defun place-point-at(col row)
+  "Places point at the given col and row coordinates.
+If the specified column is greater than the numbero of characters in a line,
+then it moves to the next line."
+  (goto-line row)
+  (beginning-of-line)
+  (forward-char col)
   )
