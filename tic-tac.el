64a0370d022d0a8e5bb65db890ac9d9799d30946
AccidentalRebel <accidentalrebel@gmail.com>
2016-06-19 22:32:44 +0800

    Created a mode that inherits from special mode
    Signed-off-by: AccidentalRebel <accidentalrebel@gmail.com>

--- tic-tac.el
+++ tic-tac.el
@@ -6,6 +6,7 @@
   "Starts the tic tac toe game"
   (interactive)
   (switch-to-buffer "tic-tac-toe")
+  (tictactoe-mode)
   (tic-tac-setup-board)
   (setq tic-tac-current-player-number 1)
   (erase-buffer)
@@ -16,20 +17,21 @@
   "Marks the point with player character"
   (interactive)
   (catch 'exit-fun
-    (let ((current-col (ebacs-get-col-at-point))
-          (current-row (ebacs-get-row-at-point)))
-      (if (char-equal (ebacs-get-char-at-coordinate current-col current-row) ?.)
-          (message "Is period")
-        (progn
-          (message "Is not period")
-          (throw 'exit-fun 0))))
+    (let ((inhibit-read-only t))
+      (let ((current-col (ebacs-get-col-at-point))
+            (current-row (ebacs-get-row-at-point)))
+        (if (char-equal (ebacs-get-char-at-coordinate current-col current-row) ?.)
+            (message "Is period")
+          (progn
+            (message "Is not period")
+            (throw 'exit-fun 0))))
 
-    (delete-char 1)
-    (if (= tic-tac-current-player-number 1)
-        (insert "X")
-      (insert "O"))
+      (delete-char 1)
+      (if (= tic-tac-current-player-number 1)
+          (tic-tac-insert-at-point "X")
+        (tic-tac-insert-at-point "O"))
 
-    (setq tic-tac-current-player-number (tic-tac-switch-to-next-player tic-tac-current-player-number))))
+      (setq tic-tac-current-player-number (tic-tac-switch-to-next-player tic-tac-current-player-number)))))
 
 ;; Setup
 (defun tic-tac-setup-board()
@@ -39,21 +41,23 @@
 (defun tic-tac-display-board()
   "Displays the board"
   (message "displaying the board")
-  (dotimes (rowIndex tic-tac-board-col-count)
-    (dotimes (colIndex tic-tac-board-row-count)
-      (insert "."))
-    (open-line 1)
-    (next-line)
-    ))
+  (let ((inhibit-read-only t))
+    (dotimes (rowIndex tic-tac-board-col-count)
+      (dotimes (colIndex tic-tac-board-row-count)
+        (tic-tac-insert-at-point "."))
+      (open-line 1)
+      (next-line)
+    )))
 
-;; VARIABLES
+;; DEFINITIONS 
+(define-derived-mode tictactoe-mode special-mode "tic-tac-toe")
+
 (defvar-local tic-tac-board 
   "This Is the tic tac toe board vector")
 
 (defvar-local tic-tac-current-player-number 1
   "The current player number")
 
-;; CONSTANTS
 (defconst tic-tac-board-col-count 3
   "The number of columns for the board")
 
@@ -68,6 +72,11 @@
       (setq tic-tac-current-player-number 1))
   (message "Current player is: %d" tic-tac-current-player-number)
   tic-tac-current-player-number)
+
+(defun tic-tac-insert-at-point (to-insert)
+  "Inserts at point but makes sure that the readonly is inhibited"
+  (let ((inhibit-read-only t))
+    (insert to-insert)))
 
 ;; TESTS
 (ert-deftest test-tic-tac-switch-to-next-player()
